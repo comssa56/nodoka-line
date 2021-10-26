41ef6354dd2f052c8fcea06fd88603e72c78a94f
DESKTOP-J2TPNSA\krkrc <comssa56@gmail.com>
2021-10-26 17:57:54 +0900

    mod: devide source file for consume functions

---
+++ src/nodoka-line/nodoka-line-consume.js
@@ -0,0 +1,123 @@
+const util = require('../util/util.js');
+const nodoka = require('./nodoka-brain.js')
+const dao_consume = require('../model/dao_consume.js');
+const line_client = require('./nodoka-line-client.js').Create();
+
+
+// 消費を保存する
+exports.handleConsume = async (ev, messages) => {
+    const kind = messages[0];
+    const price = messages[1];
+    const day = messages[2];
+
+    if(kind && price) 
+    {
+        if(!util.isIntStr(price)) {
+            return line_client.replyMessage(
+                ev.replyToken, 
+                nodoka.createNodokaTextMessage("価格は半角数字だけで記載してください")
+            );    
+        }
+
+
+        if(day)
+        {
+            const d = util.ShortStrDate(day);
+            if(day.length!=8 || !d) {
+                return line_client.replyMessage(
+                    ev.replyToken, 
+                    nodoka.createNodokaTextMessage("日付は半角数字8桁で入力してください")
+                );    
+            }
+
+            await dao_consume.insertConsumeWithDate(kind, price, d.get().format());
+            return line_client.broadcast(
+                nodoka.createNodokaTextMessage(messages + "\nを保存完了")
+            );    
+            
+        }
+        else
+        {
+            await dao_consume.insertConsume(kind, price);
+            return line_client.broadcast(
+                nodoka.createNodokaTextMessage(messages + "\nを保存完了")
+            );    
+        }
+    } else {
+        return line_client.replyMessage(
+            ev.replyToken, 
+            nodoka.createNodokaTextMessage("理解できなんだ\n\n消費\n食料orその他\n価格（半角数値）\nで入力する")
+        );
+    }
+}
+
+exports.handleConsumeStat = async (ev, messages) => {
+    const results = await dao_consume.selectConsumeSum();
+    console.log(results);
+
+    if(results) {
+        let str ="";
+        for(row of results) {
+            str += row.kind + "\t" + row.sum + "円\t" + row.date + "\n";
+        }
+        return line_client.replyMessage(ev.replyToken, 
+            nodoka.createNodokaTextMessage(str)
+            ); 
+    
+    } else {
+        return line_client.replyMessage(ev.replyToken, 
+            nodoka.createNodokaTextMessage("記録がありません")
+        ); 
+    }
+
+}
+
+exports.handleConsumeReceipt = async(ev, messages) => {
+    const yearmonth = messages[1];
+
+    if(!yearmonth || yearmonth.length!=6 || !util.ShortStrDate(yearmonth)) {
+        return line_client.replyMessage(
+            ev.replyToken, 
+            nodoka.createNodokaTextMessage("確認年月は半角数字6桁で入力してください")
+        );    
+    }
+
+    const results = await dao_consume.selectConsumeReceipt(yearmonth);
+    console.log(results);
+
+    if(results) {
+        let str ="";
+        for(row of results) {
+            str += "消費" + row.id + "\t" + row.kind + "\t" + row.date + "日\t" + row.price + "円\n";
+        }
+        return line_client.replyMessage(ev.replyToken, 
+            nodoka.createNodokaTextMessage(str)
+        ); 
+    
+    } else {
+        return line_client.replyMessage(ev.replyToken, 
+            nodoka.createNodokaTextMessage("記録がありません")
+        ); 
+    }
+
+}
+
+exports.handleDeleteReceipt = async(ev, messages) => {
+    console.log(messages[1]);
+    const id = Number(messages[1]);
+    console.log(id);
+
+    if(!id) {
+        return line_client.replyMessage(ev.replyToken, 
+            nodoka.createNodokaTextMessage("idの指定は整数で行いましょう")
+        ); 
+    
+        return;
+    }
+
+    const results = await dao_consume.deleteConsumeReceipt(id);
+    console.log(results);
+    return line_client.replyMessage(ev.replyToken, 
+        nodoka.createNodokaTextMessage("" + id + "の取り消しをしました")
+    ); 
+}
--- src/nodoka-line/nodoka-line.js
+++ src/nodoka-line/nodoka-line.js
@@ -1,127 +1,7 @@
 const result = require('../util/result.js');
-const util = require('../util/util.js');
 const nodoka = require('./nodoka-brain.js')
-const dao_consume = require('../model/dao_consume.js');
 const line_client = require('./nodoka-line-client.js').Create();
-
-
-// 消費を保存する
-async function handleConsume(ev, messages) {
-    const kind = messages[0];
-    const price = messages[1];
-    const day = messages[2];
-
-    if(kind && price) 
-    {
-        if(!util.isIntStr(price)) {
-            return line_client.replyMessage(
-                ev.replyToken, 
-                nodoka.createNodokaTextMessage("価格は半角数字だけで記載してください")
-            );    
-        }
-
-
-        if(day)
-        {
-            const d = util.ShortStrDate(day);
-            if(day.length!=8 || !d) {
-                return line_client.replyMessage(
-                    ev.replyToken, 
-                    nodoka.createNodokaTextMessage("日付は半角数字8桁で入力してください")
-                );    
-            }
-
-            await dao_consume.insertConsumeWithDate(kind, price, d.get().format());
-            return line_client.broadcast(
-                nodoka.createNodokaTextMessage(messages + "\nを保存完了")
-            );    
-            
-        }
-        else
-        {
-            await dao_consume.insertConsume(kind, price);
-            return line_client.broadcast(
-                nodoka.createNodokaTextMessage(messages + "\nを保存完了")
-            );    
-        }
-    } else {
-        return line_client.replyMessage(
-            ev.replyToken, 
-            nodoka.createNodokaTextMessage("理解できなんだ\n\n消費\n食料orその他\n価格（半角数値）\nで入力する")
-        );
-    }
-}
-
-async function handleConsumeStat(ev, messages) {
-    const results = await dao_consume.selectConsumeSum();
-    console.log(results);
-
-    if(results) {
-        let str ="";
-        for(row of results) {
-            str += row.kind + "\t" + row.sum + "円\t" + row.date + "\n";
-        }
-        return line_client.replyMessage(ev.replyToken, 
-            nodoka.createNodokaTextMessage(str)
-            ); 
-    
-    } else {
-        return line_client.replyMessage(ev.replyToken, 
-            nodoka.createNodokaTextMessage("記録がありません")
-        ); 
-    }
-
-}
-
-async function handleConsumeReceipt(ev, messages) {
-    const yearmonth = messages[1];
-
-    if(!yearmonth || yearmonth.length!=6 || !util.ShortStrDate(yearmonth)) {
-        return line_client.replyMessage(
-            ev.replyToken, 
-            nodoka.createNodokaTextMessage("確認年月は半角数字6桁で入力してください")
-        );    
-    }
-
-    const results = await dao_consume.selectConsumeReceipt(yearmonth);
-    console.log(results);
-
-    if(results) {
-        let str ="";
-        for(row of results) {
-            str += "消費" + row.id + "\t" + row.kind + "\t" + row.date + "日\t" + row.price + "円\n";
-        }
-        return line_client.replyMessage(ev.replyToken, 
-            nodoka.createNodokaTextMessage(str)
-        ); 
-    
-    } else {
-        return line_client.replyMessage(ev.replyToken, 
-            nodoka.createNodokaTextMessage("記録がありません")
-        ); 
-    }
-
-}
-
-async function handleDeleteReceipt(ev, messages) {
-    console.log(messages[1]);
-    const id = Number(messages[1]);
-    console.log(id);
-
-    if(!id) {
-        return line_client.replyMessage(ev.replyToken, 
-            nodoka.createNodokaTextMessage("idの指定は整数で行いましょう")
-        ); 
-    
-        return;
-    }
-
-    const results = await dao_consume.deleteConsumeReceipt(id);
-    console.log(results);
-    return line_client.replyMessage(ev.replyToken, 
-        nodoka.createNodokaTextMessage("" + id + "の取り消しをしました")
-    ); 
-}
+const consume = require('./nodoka-line-consume.js');
 
 async function handleEvent(ev) {
     
@@ -134,17 +14,17 @@ async function handleEvent(ev) {
     case "光熱費":
     case "日用品":
     case "育児":
-            console.log("message consume");
-        return handleConsume(ev, messages).await;
+        console.log("message consume");
+        return consume.handleConsume(ev, messages).await;
     case "消費確認":
         console.log("message consume stat");
-        return handleConsumeStat(ev, messages).await;
+        return consume.handleConsumeStat(ev, messages).await;
     case "消費明細":
         console.log("message consume receipt");
-        return handleConsumeReceipt(ev, messages).await;
+        return consume.handleConsumeReceipt(ev, messages).await;
     case "消費削除":
         console.log("message delete receipt");
-        return handleDeleteReceipt(ev, messages).await;
+        return consume.handleDeleteReceipt(ev, messages).await;
     default:
         console.log("message default");
     }
